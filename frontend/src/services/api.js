4febdd5cb4dffd1f6b7929b04bf73ecd36a300f0
Shashika071 <dhanapa-im21086@stu.kln.ac.lk>
2025-09-23 10:39:21 +0530

    axax

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://crexline.me';
+const API_BASE_URL = 'https://crexline.me/api';
  
 // Create axios instance with default config
 const api = axios.create({
