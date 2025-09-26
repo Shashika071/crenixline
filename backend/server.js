7cf91279bf67fdb96c06e52452bc82d8d86ca28c
Shashika071 <dhanapa-im21086@stu.kln.ac.lk>
2025-09-26 23:33:53 +0530

    asdasd

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD', 'CONNECT', 'TRACE', 'COPY', 'LOCK', 'MKCOL', 'MOVE', 'PROPFIND', 'PROPPATCH', 'UNLOCK', 'REPORT', 'CHECKOUT', 'MERGE', 'MSEARCH', 'NOTIFY', 'SUBSCRIBE', 'UNSUBSCRIBE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
